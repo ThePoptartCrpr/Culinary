e0f53915ac14993220f4b226018e57464093e087
FalseHonesty <skipboman0@gmail.com>
2018-06-17 17:00:02 -0400

    Cleanup & update to MC 1.12

--- src/main/java/com/thepoptartcrpr/culinary/Culinary.java
+++ src/main/java/com/thepoptartcrpr/culinary/Culinary.java
@@ -1,14 +1,13 @@
-package thepoptartcrpr.culinary;
+package com.thepoptartcrpr.culinary;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Culinary.MODID, name = Culinary.NAME, version = Culinary.VERSION)
 public class Culinary {
-
     public static final String MODID = "culinary";
     public static final String NAME = "Culinary";
     public static final String VERSION = "0.0.1";
