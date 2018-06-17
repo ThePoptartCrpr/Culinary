7299d2ce6cb7cf9bc24c5b8afd1d8c02a9d38ca1
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-17 12:27:11 -0700

    Base mod class

---
+++ src/main/java/thepoptartcrpr/culinary/Culinary.java
@@ -0,0 +1,31 @@
+package thepoptartcrpr.culinary;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Culinary.MODID, name = Culinary.NAME, version = Culinary.VERSION)
+public class Culinary {
+
+    public static final String MODID = "culinary";
+    public static final String NAME = "Culinary";
+    public static final String VERSION = "0.0.1";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
