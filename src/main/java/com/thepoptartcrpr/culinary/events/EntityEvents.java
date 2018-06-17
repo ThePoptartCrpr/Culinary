bfe59aed1de35891a55fa88289d075bb90ba5e76
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-17 13:46:55 -0700

    Added baby check to bone drops
    Baby animals should no longer drop bones

--- src/main/java/com/thepoptartcrpr/culinary/events/EntityEvents.java
+++ src/main/java/com/thepoptartcrpr/culinary/events/EntityEvents.java
@@ -1,6 +1,7 @@
 package com.thepoptartcrpr.culinary.events;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.*;
 import net.minecraft.init.Items;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
@@ -13,8 +14,9 @@ public class EntityEvents {
 
     @SubscribeEvent
     public void onEntityDrop(LivingDropsEvent event) {
-        if (event.entityLiving instanceof EntityAnimal) {
-            event.entityLiving.dropItem(Items.bone, random.nextInt(3));
+        EntityLivingBase entityLiving = event.entityLiving;
+        if (entityLiving instanceof EntityAnimal) {
+            if (((EntityAnimal) entityLiving).getGrowingAge() == 0) entityLiving.dropItem(Items.bone, random.nextInt(3));
         }
     }
 
