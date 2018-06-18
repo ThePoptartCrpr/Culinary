40cdaea29a35a15552e6bcea0d34e8d48a26ccd6
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-18 10:21:57 -0700

    unbroke creative tabs

--- src/main/java/com/thepoptartcrpr/culinary/Culinary.java
+++ src/main/java/com/thepoptartcrpr/culinary/Culinary.java
@@ -5,6 +5,7 @@ import com.thepoptartcrpr.culinary.events.EntityEvents;
 import com.thepoptartcrpr.culinary.init.CBlocks;
 import com.thepoptartcrpr.culinary.init.CItems;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -19,7 +20,19 @@ public class Culinary {
     public static final String NAME = "Culinary";
     public static final String VERSION = "0.0.1";
 
-    public static final CreativeTabs tabIngredients = new TabCIngredients();
+    public static final CreativeTabs tabIngredients = new CreativeTabs("cingredients") {
+
+        @Override
+        public ItemStack getTabIconItem() {
+            return new ItemStack(CItems.salt);
+        }
+
+        @Override
+        public boolean hasSearchBar() {
+            return true;
+        }
+
+    }.setBackgroundImageName("item_search.png");
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
