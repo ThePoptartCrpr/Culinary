6982c80fffacc6282bd7c34a24e9ddd0f3daf385
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-18 12:18:40 -0700

    Added proxies

---
+++ src/main/java/com/thepoptartcrpr/culinary/proxy/ClientProxy.java
@@ -0,0 +1,15 @@
+package com.thepoptartcrpr.culinary.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit() {
+        super.preInit();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+}
---
+++ src/main/java/com/thepoptartcrpr/culinary/proxy/CommonProxy.java
@@ -0,0 +1,13 @@
+package com.thepoptartcrpr.culinary.proxy;
+
+public class CommonProxy {
+
+    public void preInit() {
+
+    }
+
+    public void init() {
+
+    }
+
+}
