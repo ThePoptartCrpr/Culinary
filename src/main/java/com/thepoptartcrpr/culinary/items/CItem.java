8ee3411f0f1890512194f8a3e90951642c0db76d
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-20 14:35:34 -0700

    Updated CItem to use a ResourceLocation instead of a String for RegistryName

--- src/main/java/com/thepoptartcrpr/culinary/items/CItem.java
+++ src/main/java/com/thepoptartcrpr/culinary/items/CItem.java
@@ -1,12 +1,14 @@
 package com.thepoptartcrpr.culinary.items;
 
+import com.thepoptartcrpr.culinary.Culinary;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 
 public class CItem extends Item {
 
     public CItem(String name) {
         this.setUnlocalizedName(name);
-        this.setRegistryName(name);
+        this.setRegistryName(new ResourceLocation(Culinary.Reference.MODID, name));
     }
 
 }
