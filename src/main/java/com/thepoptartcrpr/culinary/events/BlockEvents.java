075fc140c4de415a8af2417ab7c66bbec803a993
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-06-21 12:30:05 -0700

    Planks now only drop from logs when player is holding saw

--- src/main/java/com/thepoptartcrpr/culinary/events/BlockEvents.java
+++ src/main/java/com/thepoptartcrpr/culinary/events/BlockEvents.java
@@ -1,6 +1,7 @@
 package com.thepoptartcrpr.culinary.events;
 
 import com.thepoptartcrpr.culinary.handlers.ConfigHandler;
+import com.thepoptartcrpr.culinary.init.CTools;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
@@ -15,8 +16,7 @@ public class BlockEvents {
     public void onBlockDrop(BlockEvent.HarvestDropsEvent event) {
         Block block = event.getState().getBlock();
         ItemStack heldItem = event.getHarvester().getHeldItemMainhand();
-        // TODO: make this also check if player is holding saw item once implemented
-        if (block == Blocks.LOG && ConfigHandler.CONFIG.shouldSawLogs) {
+        if (block == Blocks.LOG && ConfigHandler.CONFIG.shouldSawLogs && heldItem.isItemEqualIgnoreDurability(new ItemStack(CTools.saw))) {
             // TODO: improve functionality of below remove statement
             event.getDrops().remove(event.getDrops().get(0));
             // TODO: drop plank of correct type according to log type
